045ba99c57209aa6fe0810b3799e9af74438471c
Alessio Civitillo <@acivitillo@dyvenia.com>
2021-09-26 18:40:49 +0200

    adding sqlite db support

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from models import Epic
+from sqlmodel import Session, select
+from models import Epic, TimeLog, engine
+import datetime
+
+# strftime("%m/%d/%Y, %H:%M:%S")
 
 app = FastAPI()
+session = Session(engine)
 
 # example of url value to python function value
 @app.get("/hello/{name}")
@@ -15,3 +20,30 @@ async def epics():
     epic2 = Epic(label="HyperlocalNW", value=2)
     epic3 = Epic(label="MarketingKPIs", value=3)
     return [epic1, epic2, epic3]
+
+
+@app.get("/api/timelogs")
+async def get_timelogs():
+    statement = select(TimeLog)
+    results = session.exec(statement).all()
+    out = []
+    print(results)
+    for result in results:
+        start_time = datetime.datetime.strptime(
+            # 2021-09-26T17:25:00.000Z
+            result.start_time,
+            "%Y-%m-%dT%H:%M:%S.%fz",
+        )
+        msg = f"""epic_id is {result.epic_id}
+                , start date {start_time.strftime("%m/%d/%Y, %H:%M:%S")} 
+                and end date {result.end_time}
+                """
+        d = result.dict()
+        d["message"] = msg
+        out.append(d)
+    return out
+
+
+@app.post("/api/timelog/")
+async def timelog(timelog: TimeLog):
+    session.add(timelog)
--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,29 @@
-from pydantic import BaseModel
+from typing import Optional
+from sqlmodel import Field, SQLModel, Field, create_engine
+
+sqlite_file_name = "database_.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
 
 
-class Epic(BaseModel):
+class Epic(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     label: str
     value: int
     disabled: bool = False
+
+
+class TimeLog(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    epic_id: int
+    start_time: str
+    end_time: str
+
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    # create_db()
+    pass
