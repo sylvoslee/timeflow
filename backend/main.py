e0886744839b407fce81bc5c494c264ad9366184
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2021-11-22 12:08:52 +0100

    html_dominate tests

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import *
 from sqlmodel import Session, select, SQLModel
 from sqlalchemy.exc import OperationalError
 from models import Epic, TimeLog, User, engine, create_db, Client
 from utils import string_to_datetime
+from fastapi.responses import HTMLResponse
 import datetime
+from dominate.tags import table, tr, td, style, div, thead, tbody, strong
 
 # strftime("%m/%d/%Y, %H:%M:%S")
 
@@ -82,6 +84,7 @@ async def hello_name(name):
 
 
 # For dashboard page
+
 # Get timelogs
 @app.get("/api/timelogs")
 async def get_timelogs():
@@ -112,34 +115,92 @@ async def get_timelogs():
     return out
 
 
-# For Developer Dashboard page
-# Get work hours for developer dashboard (ddashboard)
-@app.get("/api/ddashboard/work_days/{select_initials},{select_month},{select_epic}")
+# For Developer Dashboard page (ddashboard)
+
+# Get months numbers in a strings list
+@app.get("/api/months")
+async def get_months():
+    months_list = []
+    for i in range(1, 13):
+        j = str(i)
+        months_list.append(j)
+    return months_list
+
+
+# HTML testing table
+@app.get("/html/table_test", response_class=HTMLResponse)
+async def get_html():
+    _table = table(border="1", style="border-collapse:collapse;border-spacing:2")
+    _thead, _tbody = _table.add(thead(), tbody())
+    _tr = _thead.add(tr())
+    with _tr:
+        td(strong("user id"))
+        td("month")
+        td("epic")
+        td("days worked")
+    _tr2 = _tbody.add(tr())
+    with _tr2:
+        td("user%id")
+        td("%month")
+        td("%epic")
+        td("%days worked")
+
+    return _table.render()
+
+
+# Get work hours for Developer Dashboard (ddashboard)
+@app.get(
+    "/api/ddashboard/table/{user_initials},{month},{epic_name}",
+    response_class=HTMLResponse,
+)
 async def get_ddashboard_work_days(
-    select_initials: str, select_month: str, select_epic: str
+    user_initials: str,
+    month: str,
+    epic_name: str,
 ) -> str:
 
-    select_month_cut = select_month[:33]
-    monthstr_to_dt = string_to_datetime_GMT(select_month_cut)
-    month_from_dt = monthstr_to_dt.month
-
+    # searching for work_days with selected arguments
     statement = (
         select(TimeLog.work_hours)
-        .where(TimeLog.user_initials == select_initials)
-        .where(TimeLog.month == month_from_dt)
-        .where(TimeLog.epic_name == select_epic)
+        .where(TimeLog.user_initials == user_initials)
+        .where(TimeLog.month == month)
+        .where(TimeLog.epic_name == epic_name)
     )
     work_hours_list = []
     results = session.exec(statement).all()
     for result in results:
         work_hours_list.append(result)
-
-    work_days = sum(work_hours_list) / 8
+    work_days = "{:.2f}".format(sum(work_hours_list) / 8)
     work_days_str = str(work_days)
-    return work_days_str
+
+    # HTML table for ddashboard
+    _table = table(
+        border="1",
+        style="overflow:hidden;padding:10px 5px;border-collapse:collapse;border-spacing:2;font-size:15px",
+        cellpadding="10",
+    )
+    _thead, _tbody = _table.add(thead(), tbody())
+    _tr = _thead.add(
+        tr(
+            style="border-color:inherit;text-align:center;vertical-align:top;background-color: #96D4D4"
+        )
+    )
+    with _tr:
+        td(strong("user id"))
+        td(strong("month"))
+        td(strong("epic"))
+        td(strong("days worked"))
+    _tr2 = _tbody.add(tr())
+    with _tr2:
+        td("%s" % user_initials)
+        td("%s" % month)
+        td("%s" % epic_name)
+        td(work_days_str)
+    return _table.render()
 
 
 # For Work Time page
+
 # Get TimeLog by user_id, epic_name and time period
 @app.get("/api/timelogs/{user_id},{epic_name},{start_time},{end_time}")
 async def get_user_by_epic_name(user_id, epic_name, start_time, end_time):
