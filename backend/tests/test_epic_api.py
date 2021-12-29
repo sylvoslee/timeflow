2b8a4ff2c24642deb826bb95bb453cbdeb9adb07
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2021-12-29 18:20:23 +0100

    test /api/epics/{epic_name}

--- backend/tests/test_epic_api.py
+++ backend/tests/test_epic_api.py
@@ -7,3 +7,8 @@ client = TestClient(app)
 def test_get_epic_list():
     response = client.get("/api/epics/")
     assert response.status_code == 200
+
+
+def test_get_epic_list():
+    response = client.get("/api/epics/{epic_name}")
+    assert response.status_code == 200
