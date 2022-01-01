015e030f5a3a7feb8f79429640869bd1465689b7
Alessio Civitillo <@acivitillo@dyvenia.com>
2022-01-01 09:49:36 +0100

    adding setup/teardown db to test_epic

--- backend/tests/test_epic_api.py
+++ backend/tests/test_epic_api.py
@@ -1,31 +1,47 @@
 from fastapi.testclient import TestClient
+import pytest
+import os
 from ..main import app, session
 from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel.pool import StaticPool
 from ..api.epic import get_session
 
-test_con = f"sqlite:///backend/tests/test_db.sqlite"
+db_name = "test_db.sqlite"
+test_con = f"sqlite:///{db_name}"
 test_engine = create_engine(
     test_con, connect_args={"check_same_thread": False}, echo=True
 )
 
-
-def get_session_override():
-    session = Session(test_engine)
-    return session
-
-
-client = TestClient(app)
-
-
-def create_test_db():
+# Creates the DB and Deletes it after ALL tests are run thanks to scope=module
+@pytest.fixture(name="create_db", scope="module")
+def create_db():
+    # setup
     SQLModel.metadata.create_all(test_engine)
-    return True
+    yield
+    # teardown
+    os.remove(db_name)
 
 
-def test_post_epic():
-    create_test_db()
+@pytest.fixture(name="session")
+def session_fixture(create_db):
+    create_db
+
+    with Session(test_engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
     app.dependency_overrides[get_session] = get_session_override
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
 
+
+def test_post_epic(client):
     response = client.post(
         "/api/epics/",
         json={"name": "[dyvenia]branding", "work_area": "DYV", "client_id": 1},
@@ -42,10 +58,8 @@ def test_post_epic():
     }
 
 
-def test_get_epic_list():
-    app.dependency_overrides[get_session] = get_session_override
+def test_get_epic_list(client):
     response = client.get("/api/epics/")
-    app.dependency_overrides.clear()
     data = response.json()
     assert response.status_code == 200
     assert data == [
@@ -53,8 +67,6 @@ def test_get_epic_list():
     ]
 
 
-def test_delete_epics():
-    app.dependency_overrides[get_session] = get_session_override
+def test_delete_epics(client):
     response = client.delete("/api/epics/?epic_name=[dyvenia]branding")
     assert response.status_code == 200
-    app.dependency_overrides.clear()
