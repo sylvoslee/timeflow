79460714fc126c547b70ca2e5fb88b77afcc6374
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2022-01-21 17:06:22 +0100

    validators added to the timelog model

--- backend/models/timelog.py
+++ backend/models/timelog.py
@@ -5,7 +5,7 @@ from pydantic import validator, root_validator, ValidationError
 from datetime import datetime, timedelta
 from ..utils import string_to_datetime
 
-# """
+
 # {
 #   "user_id": 1,
 #   "start_time": "2022-01-19T08:30:00.000Z",
@@ -14,11 +14,9 @@ from ..utils import string_to_datetime
 #   "epic_id": 1,
 #   "count_hours": 0,
 #   "count_days": 0,
-#   "daily_value": 0,
 #   "month": 0,
 #   "year": 0
 # }
-# """
 
 
 class TimeLog(SQLModel, table=True):
@@ -30,32 +28,33 @@ class TimeLog(SQLModel, table=True):
     epic_id: int = Field(foreign_key="epic.id")
     count_hours: float
     count_days: float
-    daily_value: float
     month: int
     year: int
 
-    # actually computed "month" field
+    @root_validator(pre=True)
+    def check_time_delta(cls, values):
+        assert (
+            values["start_time"] < values["end_time"]
+        ), "start_time can't be smaller then end_time"
+        return values
+
+    # actually computed "month" field --> to be changed
     @validator("month", always=True, pre=True)
     def set_month(cls, month_value, values):
         start_time_dt = string_to_datetime(values["start_time"])
         month_value = start_time_dt.month
         return month_value
 
-    # actually computed "year" field
+    # actually computed "year" field --> to be changed
     @validator("year", always=True, pre=True)
     def set_year(cls, year_value, values):
         start_time_dt = string_to_datetime(values["start_time"])
         year_value = start_time_dt.year
         return year_value
 
-    @validator("end_time", always=True, pre=True)
-    def check_time_delta(cls, v, values):
-        if v <= values["start_time"]:
-            raise ValueError("start_time is bigger or same as end_time")
-        return v
-
-    @root_validator
-    def count_hours_change(cls, values):
+    # actually computed "count_hours" field --> to be changed
+    @root_validator(pre=True)
+    def count_hours_compute(cls, values):
         delta = string_to_datetime(values["end_time"]) - string_to_datetime(
             values["start_time"]
         )
@@ -64,7 +63,25 @@ class TimeLog(SQLModel, table=True):
         values["count_hours"] = work_hours
         return values
 
+    # actually computed "count_days" field --> to be changed
+    @root_validator(pre=True)
+    def count_days_compute(cls, values):
+        delta = string_to_datetime(values["end_time"]) - string_to_datetime(
+            values["start_time"]
+        )
+        work_delta_days = delta.total_seconds() / 3600 / 8
+        work_days = "{:.2f}".format(work_delta_days)
+        values["count_days"] = work_days
+        return values
+
+    @validator("count_hours", always=True)
+    def daily_hours(cls, hours_input):
+        assert hours_input < 12, "developer worked over 12 hours"
+        return hours_input
+
     @validator("year", always=True)
     def valid_year(cls, year_input):
-        assert year_input >= datetime.now().year
+        assert year_input in range(
+            2021, datetime.now().year + 1
+        ), "year value not in range [2021, current year]"
         return year_input
