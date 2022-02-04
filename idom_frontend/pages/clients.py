27767ccf86e6837f10ea4d76bf8de5d8eed8c3b5
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2022-02-04 13:42:02 +0100

    clients delet api change

--- idom_frontend/pages/clients.py
+++ idom_frontend/pages/clients.py
@@ -92,7 +92,8 @@ def delete_client(is_changed, set_is_changed):
     client_name, set_client_name = use_state("")
 
     def delete_client(event):
-        api = f"{base_url}/api/clients/{client_id}?name={client_name}"
+        api = f"{base_url}/api/clients/{client_id}?client_name={client_name}"
+        print(api)
         response = requests.delete(api)
         set_is_changed(True)
 
