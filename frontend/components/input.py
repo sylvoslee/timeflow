647131d694defcf3f4e70ad399888127fdf6a5ae
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2022-02-21 16:30:49 +0100

    update

--- frontend/components/input.py
+++ frontend/components/input.py
@@ -101,10 +101,17 @@ def AutoSelect(
     )
 
 
+class_h3 = """text-primary-500 placeholder-secondary-400 w-full px-8 py-2 mt-2 
+                    text-base bg-secondary-300"""
+
+
 @component
 def DisplayValue(
     display_value: str,
     value: str,
-    _class: str = class_str,
+    class_h3: str = class_h3,
 ):
-    return html.h3({"class": "bg-white", "value": value}, display_value)
+    return html.h3(
+        {"class": class_h3, "value": value},
+        display_value,
+    )
