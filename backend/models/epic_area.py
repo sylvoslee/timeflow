245133ecffa2ad481bba8e516a8f97e0df3521d7
liberty-rising <rwyszomirski@dyvenia.com>
2022-02-26 09:21:06 +0100

    add default template for the model

---
+++ backend/models/epic_area.py
@@ -0,0 +1,15 @@
+from sqlite3.dbapi2 import Timestamp, adapt
+from typing import Optional
+from sqlmodel import Field, SQLModel
+from pydantic import validator
+from datetime import datetime
+
+
+class EpicArea(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    work_area: str
+    client_id: int = Field(foreign_key="client.id")
+    active: bool
+    created_at: datetime
+    updated_at: datetime
