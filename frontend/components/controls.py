dfcba578ff607351aa562d032055dda1c993aea1
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2022-02-28 16:40:42 +0100

    small changes

--- frontend/components/controls.py
+++ frontend/components/controls.py
@@ -1,21 +1,24 @@
+from idom import component, html
+from typing import Callable
 
 
-
-    # wrap this inside a Button component
+@component
+def SubmitButton(
+    handle_submit: Callable, user_id: int, epic_id: int, year_month: int, days: int
+):
     button_is_active = True
-    if user_id == "" or epic_id == "" or year_month == "" or days == "":
+    if user_id == "" or epic_id == "" or year_month == "":
         button_is_active = False
 
     button_status = "text-gray-50  border-secondary-200"
     if button_is_active is False:
         button_status = "text-gray-500  border-gray-500"
 
-    btn = html.button(
+    return html.button(
         {
             "class": f"relative w-fit h-fit px-2 py-1 text-lg border {button_status}",
             "onClick": handle_submit,
-            "disabled": True,
+            "disabled": button_is_active,
         },
         "Submit",
     )
-    # end of wrap
--- frontend/pages/forecasts.py
+++ frontend/pages/forecasts.py
@@ -10,7 +10,7 @@ from components.input import Input, Selector, Selector2
 from components.layout import Row, Column, Container
 from components.lists import ListSimple
 from components.table import SimpleTable
-
+from components.controls import SubmitButton
 from pages.data import (
     epics_names,
     client_name_by_epic_id,
@@ -133,23 +133,25 @@ def create_forecast_form(
     )
 
     # wrap this inside a Button component
-    button_is_active = True
-    if user_id == "" or epic_id == "" or year_month == "" or days == "":
-        button_is_active = False
+    # button_is_active = True
+    # if user_id == "" or epic_id == "" or year_month == "" or days == "":
+    #     button_is_active = False
 
-    button_status = "text-gray-50  border-secondary-200"
-    if button_is_active is False:
-        button_status = "text-gray-500  border-gray-500"
+    # button_status = "text-gray-50  border-secondary-200"
+    # if button_is_active is False:
+    #     button_status = "text-gray-500  border-gray-500"
 
-    btn = html.button(
-        {
-            "class": f"relative w-fit h-fit px-2 py-1 text-lg border {button_status}",
-            "onClick": handle_submit,
-            "disabled": True,
-        },
-        "Submit",
-    )
+    # btn = html.button(
+    #     {
+    #         "class": f"relative w-fit h-fit px-2 py-1 text-lg border {button_status}",
+    #         "onClick": handle_submit,
+    #         "disabled": True,
+    #     },
+    #     "Submit",
+    # )
     # end of wrap
+
+    btn = SubmitButton(handle_submit, user_id, epic_id, year_month, days)
 
     return Column(
         Row(
