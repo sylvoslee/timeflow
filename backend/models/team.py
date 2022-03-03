7fcfdbf7d37ebab53d815b087a2f97da8307b4c3
liberty-rising <rwyszomirski@dyvenia.com>
2022-03-03 09:35:39 +0100

    build team model

---
+++ backend/models/team.py
@@ -0,0 +1,16 @@
+# id
+# short_name
+# name
+# user_id
+from typing import Optional
+from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+
+class Team(SQLModel, table=True):
+    """Create an SQLModel for teams"""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    name: str
+    short_name: str
