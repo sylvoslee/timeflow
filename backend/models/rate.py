97f497e3ad81c6d6b2ac7bfbc1429bf462061123
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2022-03-03 17:34:39 +0100

    model update

--- backend/models/rate.py
+++ backend/models/rate.py
@@ -2,15 +2,15 @@ from sqlite3.dbapi2 import Timestamp, adapt
 from typing import Optional
 from sqlmodel import Field, SQLModel
 from pydantic import validator
-from datetime import datetime
+from datetime import datetime, date
 
 
 class Rate(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     client_id: int = Field(foreign_key="client.id")
-    year: int
-    month: int
+    valid_from: date
+    valid_to: date
     amount: float  # currency: EUR
     created_at: datetime
     updated_at: datetime
