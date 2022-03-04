18628abcbfc2db6a74c284e094fffd072a708ab8
liberty-rising <rwyszomirski@dyvenia.com>
2022-03-04 08:36:20 +0100

    create sponsors page and add default layout from epic areas page

---
+++ frontend/pages/sponsors.py
@@ -0,0 +1,18 @@
+import asyncio
+from cProfile import label
+import json
+from idom import html, use_state, component, event
+import requests
+from sanic import response
+from black import click
+from datetime import datetime
+
+from components.input import Input, SelectorDropdownKeyValue, Selector
+from components.layout import Row, Column, Container
+from components.table import SimpleTable
+from config import base_url
+
+
+@component
+def page():
+    pass
