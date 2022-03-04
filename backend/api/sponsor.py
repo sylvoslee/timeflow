760b5951c06e5146006dab16da36d72688d973fb
liberty-rising <rwyszomirski@dyvenia.com>
2022-03-04 08:25:31 +0100

    add post method to sponsor api

---
+++ backend/api/sponsor.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends
+from ..utils import engine, get_session
+from sqlmodel import Session, select, SQLMOdel, or_
+from ..models.client import Client
+from ..models.sponsor import Sponsor
+from sqlalchemy.exc import NoResultFound
+from datetime import datetime
+
+router = APIRouter(prefix="/api/sponsors", tags=["sponsor"])
+session = Session(engine)
+
+# Post new sponsor
+@router.post("/")
+async def post_sponsor(
+    *,
+    sponsor: Sponsor,
+    session: Session = Depends(get_session),
+):
+    statement = select(Sponsor).where(
+        or_(Sponsor.name == sponsor.name, Sponsor.id == sponsor.id)
+    )
+    try:
+        result = session.exec(statement).one()
+        return False
+    except NoResultFound:
+        session.add(sponsor)
+        session.commit()
+        session.refresh(sponsor)
+        return sponsor
