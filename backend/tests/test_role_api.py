54e3c6cc5abc438cd82a3d1b7bdce36cf56f8b2d
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2022-03-07 11:24:04 +0100

    added role api tests

--- backend/tests/test_role_api.py
+++ backend/tests/test_role_api.py
@@ -11,12 +11,12 @@ from datetime import datetime
 @pytest.mark.order(1)
 def test_post_role(client):
     response = client.post(
-        "/api/roles",
+        "/api/roles/",
         json={
-            "short_name": "asd",
+            "short_name": "sda",
             "name": "Senior Data Analyst",
-            "created_at": "2022-03-04T16:52:51.332Z",
-            "updated_at": "2022-03-04T16:52:51.332Z",
+            "created_at": "2022-03-07T10:00:50.800Z",
+            "updated_at": "2022-03-07T10:00:50.800Z",
             "is_active": True,
         },
     )
@@ -24,58 +24,42 @@ def test_post_role(client):
     assert response.status_code == 200
     assert data == {
         "id": 1,
-        "short_name": "asd",
-        "created_at": "2022-03-04T16:52:51.332000",
+        "short_name": "sda",
+        "created_at": "2022-03-07T10:00:50.800000",
         "is_active": True,
         "name": "Senior Data Analyst",
-        "updated_at": "2022-03-04T16:52:51.332000",
+        "updated_at": "2022-03-07T10:00:50.800000",
     }
 
 
-# def test_read_teams(client):
-#     response = client.get("/api/teams/juniors")
-#     data = response.json()
-#     assert response.status_code == 200
-#     assert data == {
-#         "id": 1,
-#         "user_id": 1,
-#         "name": "juniors",
-#         "short_name": "jrs",
-#         "is_active": True,
-#     }
+def test_read_roles(client):
+    response = client.get(
+        "/api/roles/",
+        json=[
+            {
+                "id": 1,
+                "short_name": "sda",
+                "created_at": "2022-03-07T10:00:50.800000",
+                "is_active": True,
+                "name": "Senior Data Analyst",
+                "updated_at": "2022-03-07T10:00:50.800000",
+            }
+        ],
+    )
 
 
-# def test_update_team(client):
-#     response = client.put(
-#         "api/teams/?id=1&name=juniors&short_name=jrs&active=True&new_user_id=2"
-#     )
-#     data = response.json()
-#     assert response.status_code == 200
-#     assert data == {
-#         "id": 1,
-#         "user_id": 2,
-#         "name": "juniors",
-#         "short_name": "jrs",
-#         "is_active": True,
-#     }
+def test_deactivate_role(client):
+    response = client.put("/api/roles/1/deactivate")
+    assert response.status_code == 200
 
 
-# def test_get_team(client):
-#     response = client.get("/api/teams/")
-#     data = response.json()
-#     assert response.status_code == 200
-#     assert data == [
-#         {
-#             "id": 1,
-#             "user_id": 2,
-#             "name": "juniors",
-#             "short_name": "jrs",
-#             "is_active": True,
-#         }
-#     ]
+def test_activate_role(client):
+    response = client.put("/api/roles/1/activate")
+    assert response.status_code == 200
 
 
-# @pytest.mark.order(-1)
-# def test_deactivate_teams(client):
-#     response = client.put("/api/teams/juniors/deactivate")
-#     assert response.status_code == 200
+def test_update_role(client):
+    response = client.put(
+        "api/roles/?id=1&new_name=mde&new_short_name=Mid%20Data%20Engineer&is_active=true"
+    )
+    assert response.status_code == 200
