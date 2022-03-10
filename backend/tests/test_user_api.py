91ee59f3becdc1b3942ddeb66751a3d3302bac70
Jedrzej Krobicki <krobicki.jedrzej@gmail.com>
2022-03-10 11:56:30 +0100

    updated model, api and api tests

--- backend/tests/test_user_api.py
+++ backend/tests/test_user_api.py
@@ -10,41 +10,60 @@ from ..api.epic import get_session
 def test_post_user(client):
     response = client.post(
         "/api/users/",
-        json={"username": "anowak", "name": "adam", "surname": "nowak", "email": "anowak@dyvenia.com"}
+        json={
+            "short_name": "jsmith",
+            "first_name": "John",
+            "last_name": "Smith",
+            "email": "jsmith@dyvenia.com",
+            "role_id": 1,
+            "team_id": 1,
+            "start_date": "2022-03-10",
+            "created_at": "2022-03-10T10:33:43.308Z",
+            "updated_at": "2022-03-10T10:33:43.308Z",
+            "is_active": True,
+        },
     )
     data = response.json()
     assert response.status_code == 200
     assert data == {
+        "short_name": "jsmith",
+        "last_name": "Smith",
+        "role_id": 1,
+        "team_id": 1,
+        "created_at": "2022-03-10T10:33:43.308000",
+        "is_active": True,
         "id": 1,
-        "username": "anowak", 
-        "name": "adam", 
-        "surname": "nowak", 
-        "email": "anowak@dyvenia.com"
+        "email": "jsmith@dyvenia.com",
+        "first_name": "John",
+        "start_date": "2022-03-10",
+        "updated_at": "2022-03-10T10:33:43.308000",
     }
+
 
 def test_get_users(client):
     response = client.get("/api/users/")
     data = response.json()
-    assert data == [{
-        "id": 1,
-        "username": "anowak", 
-        "name": "adam", 
-        "surname": "nowak", 
-        "email": "anowak@dyvenia.com"
-    }]
+    assert data == [
+        {
+            "short_name": "jsmith",
+            "last_name": "Smith",
+            "role_id": 1,
+            "team_id": 1,
+            "created_at": "2022-03-10T10:33:43.308000",
+            "is_active": True,
+            "id": 1,
+            "first_name": "John",
+            "email": "jsmith@dyvenia.com",
+            "start_date": "2022-03-10",
+            "updated_at": "2022-03-10T10:33:43.308000",
+        }
+    ]
 
 
 def test_update_user(client):
-    response = client.put("api/users/?username=anowak&email=adam.nowak%40dyvenia.com")
-    data = response.json()
-    assert data == {
-        "id": 1,
-        "username": "anowak", 
-        "name": "adam", 
-        "surname": "nowak", 
-        "email": "adam.nowak@dyvenia.com"
-    }
-
-def test_delete_user(client):
-    response = client.delete("/api/users/?username=anowak")
-    assert response.status_code == 200
+    response1 = client.put("api/users/1/?is_active=false")
+    assert response1.status_code == 200
+    response2 = client.put("api/users/1/?is_active=true")
+    assert response2.status_code == 200
+    response3 = client.put("api/users/1/?new_email=jnowak")
+    assert response3.status_code == 200
